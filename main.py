09049792a33fc67b478df7ca6dedd04d8eb7de9d
Vaibhav Gupta <vg498660@gmail.com>
2024-10-24 18:53:26 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.Proj_1 import logger
+from src.Proj_1.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.Proj_1.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
-logger.info("Welcome to the custom created logging")
+STAGE_NAME="Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
